8fdffdfc0ce3d5998e00d5232dc3c3feaefdcf17
gandaman <kushalkumar.gandamaneni@tomtom.com>
2018-11-06 17:28:38 +0530

    some name changes

--- assignments/assignment3/assignment3-starter-code/js/app.js
+++ assignments/assignment3/assignment3-starter-code/js/app.js
@@ -9,6 +9,7 @@
 
   function FoundItems() {
     var ddo = {
+      restrict: 'E',
       templateUrl: 'items.html',
       scope: {
         items: '<',
@@ -21,16 +22,15 @@
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
-    var menu = this;
-    menu.found = [];
+    var narrowDownController = this;
+    narrowDownController.found = [];
 
-    //Implementaion for Filtering Items based on searchTerm
-    menu.getMatchedMenuItems = function () {
-      menu.found = []
-      if (menu.searchTerm) {
-        var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+    narrowDownController.getMatchedMenuItems = function () {
+    	narrowDownController.found = []
+      if (narrowDownController.searchTerm) {
+        var promise = MenuSearchService.getMatchedMenuItems(narrowDownController.searchTerm);
         promise.then(function (response) {
-          menu.found = response;
+        	narrowDownController.found = response;
         })
         .catch(function (error) {
           console.log("Error occured!!");
@@ -38,11 +38,10 @@
       }
     };
 
-    // Implementaion for removing item at given index
-    menu.removeItem = function (index) {
-      menu.found.splice(index, 1);
-      if (menu.found.length === 0) {
-        menu.error = "Nothing found";
+    narrowDownController.removeItem = function (index) {
+    	narrowDownController.found.splice(index, 1);
+      if (narrowDownController.found.length === 0) {
+    	  narrowDownController.error = "Nothing found";
       }
     }
   };
@@ -50,15 +49,14 @@
 
   MenuSearchService.$inject = ['$http', 'ApiBasePath']
   function MenuSearchService($http, ApiBasePath) {
-    var service = this;
+    var menuService = this;
 
-    service.getMatchedMenuItems = function (searchTerm) {
+    menuService.getMatchedMenuItems = function (searchTerm) {
       return $http({
         method: "GET",
         url: (ApiBasePath + "/menu_items.json"),
       }).then(function (result) {
 
-        // process result and only keep items that match
         var items = result.data.menu_items;
         var foundItems = []
         for (var index = 0; index < items.length; index++) {
@@ -67,7 +65,6 @@
           }
         }
 
-        // return processed items
         return foundItems;
       });
     };
